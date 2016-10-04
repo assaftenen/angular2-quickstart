4594b3447a9444f8df34084cec1c93b36b2ed185
Assaf <assaf.tenen@gmail.com>
2016-10-04 23:46:30 +0300

    part 6 - beginning before add routing

--- app/app.module.js
+++ app/app.module.js
@@ -12,6 +12,8 @@ var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
+var heroes_component_1 = require('./heroes.component');
+var hero_service_1 = require("./hero.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -22,7 +24,11 @@ var AppModule = (function () {
                 forms_1.FormsModule
             ],
             declarations: [
-                app_component_1.AppComponent
+                app_component_1.AppComponent,
+                heroes_component_1.HeroesComponent
+            ],
+            providers: [
+                hero_service_1.HeroService
             ],
             bootstrap: [app_component_1.AppComponent]
         }), 
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import {HeroService} from "./hero.service";
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        HeroesComponent
+    ],
+    providers:[
+      HeroService
     ],
     bootstrap: [ AppComponent ]
 })

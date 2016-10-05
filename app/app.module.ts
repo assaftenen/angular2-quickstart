c9d376275e8fc2f7c45717dfecb504eec3c073b0
Assaf <assaf.tenen@gmail.com>
2016-10-05 23:41:33 +0300

    part 6 - beginning before Dashboard Top Heroes

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent} from './dashboard.component';
 import {HeroService} from "./hero.service";
+import {routing} from './app.routing';
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        routing
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
         HeroesComponent
     ],
+
     providers:[
       HeroService
     ],

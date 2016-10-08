fa1a71d9b2a7d1e9e165f828fd03138eb2ce9ad6
Assaf <assaf.tenen@gmail.com>
2016-10-08 13:29:37 +0300

    6 - end

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,38 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { DashboardComponent} from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import {HeroService} from "./hero.service";
-import {routing} from './app.routing';
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        routing
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'detail/:id',
+                component: HeroDetailComponent
+            },
+
+        ]),
     ],
     declarations: [
         AppComponent,
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,31 +1,19 @@
-// Keep the Input import for now, we'll remove it later:
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
-import { HeroService } from './hero.service';
-import {Hero} from "./hero";
-
+import { Hero }         from './hero';
+import { HeroService }  from './hero.service';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-hero-detail',
-    template: `
-<div *ngIf="hero">
-  <h2>{{hero.name}} details!</h2>
-  <div>
-    <label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name" />
-  </div>
-  <button (click)="goBack()">Back</button>
-</div>
-`
+    templateUrl: 'hero-detail.component.html',
+    styleUrls: [ 'hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-
-    @Input()
     hero: Hero;
+
     constructor(
         private heroService: HeroService,
         private route: ActivatedRoute,
@@ -39,8 +27,8 @@ export class HeroDetailComponent implements OnInit {
                 .then(hero => this.hero = hero);
         });
     }
+
     goBack(): void {
         this.location.back();
     }
-
 }
